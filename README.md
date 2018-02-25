2ae55f10913abfcef7d6db01defb8a6c3663230e
Bitcoin-N <dev@bitcoinn.biz>
2018-02-25 18:44:13 +0100

    Update 1
    v1.0

---
+++ README.md
@@ -0,0 +1,10 @@
+#Bitcoin-N-Blockchain-Explorer
+Block explorer for Bitcoin-N (bitcoin nova) CryptoNote based cryptocurrency.
+
+#### Installation
+
+1) It takes data from daemon karbowanecd. It should be accessible from the Internet. Run karbowanecd with open port as follows:
+```bash
+./karbowanecd --restricted-rpc --enable-cors=* --enable-blockchain-indexes --rpc-bind-ip=0.0.0.0 --rpc-bind-port=32348
+```
+2) Just upload to your website and change 'api' variable in config.js to point to your daemon.
---
+++ config.js
@@ -0,0 +1,15 @@
+var api = 'http://pool.bitcoinn.biz:5454';
+var blockTargetInterval = 120;
+var coinUnits = 1000000;
+var symbol = 'BTN';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+    "btn": [
+        ["pool.bitcoinn.biz", "http://pool.bitcoinn.biz:8133"],
+        ["bitcoinn.criptominando.eu", "https://bitcoinn.criptominando.eu:8216"],
+        ["bitcoinn.multipool.es", "https://bitcoinn.multipool.es:8118"],
+        ["btn.dreampool.info", "https://btn.dreampool.info/api"],
+        ["superblockchain.con-ip.com/btn", "http://superblockchain.con-ip.com:8133"]
+    ]
+};
