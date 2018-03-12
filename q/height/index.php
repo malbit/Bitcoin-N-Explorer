08c40af0f37461497fbd23b64570ce8b60b04182
Bitcoin-N <dev@bitcoinn.biz>
2018-03-12 20:50:55 +0100

    Fix

--- q/height/index.php
+++ q/height/index.php
@@ -2,7 +2,7 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'http://45.32.232.11:32348/getinfo');
+curl_setopt($ch, CURLOPT_URL, 'http://pool.bitcoinn.biz:5454/getinfo');
 $result = curl_exec($ch);
 $obj = json_decode($result, TRUE);
 curl_close($ch);
